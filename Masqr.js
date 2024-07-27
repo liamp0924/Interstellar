9b2a8eb5eb489ce7049ca87f61fb9420663fd730
liamp0924 <111157961+liamp0924@users.noreply.github.com>
2024-07-26 21:29:15 -0400

    Update Masqr.js

--- Masqr.js
+++ Masqr.js
@@ -3,7 +3,6 @@ import path from "node:path"
 import fetch from "node-fetch"
 
 const LICENSE_SERVER_URL = "https://masqr.gointerstellar.app/validate?license="
-const Fail = fs.readFileSync("Failed.html", "utf8")
 
 export function setupMasqr(app) {
   app.use(async (req, res, next) => {
